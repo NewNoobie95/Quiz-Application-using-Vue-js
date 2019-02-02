e20291931d41851ab8d070b526d05f95311efc99
NewNoobie95 <47270674+NewNoobie95@users.noreply.github.com>
2019-02-02 16:56:53 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img width="800px" height="250px" alt="Vue logo" src="./assets/dis.png">
+    <HelloWorld msg="Know thou System"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
